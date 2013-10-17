03ae5cf6bb7dbf08091da890b106e6ff38a63679
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-10-17 06:53:40 -0400

    initial commit

---
+++ Color.js
@@ -0,0 +1,13 @@
+function Color() {
+}
+
+Color.BLACK = "Black";
+Color.BLUE = "Blue";
+Color.CYAN = "Cyan";
+Color.GREEN = "Green";
+Color.MAGENTA = "Magenta";
+Color.ORANGE = "Orange";
+Color.PINK = "Pink";
+Color.RED = "Red";
+Color.YELLOW = "Yellow";
+Color.NONE = "None";
---
+++ Tile.js
@@ -0,0 +1,91 @@
+function Tile(tile) {
+   this.getPoint = Tile_getPoint;
+   this.setPoint = Tile_setPoint;
+
+   this.getColor = Tile_getColor;
+   this.setColor = Tile_setColor;
+
+   this.attachTo = Tile_attachTo;
+   this.detachFrom = Tile_detachFrom;
+
+   this.setXCoordinate = Tile_setXCoordinate;
+   this.setYCoordinate = Tile_setYCoordinate;
+
+   this.onClick = Tile_onClick;
+
+   this.rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+
+   this.rect.setAttribute("stroke", Color.BLACK);
+   this.rect.setAttribute("strokeWidth", "1");
+
+   this.rect.setAttribute("width", 20);
+   this.rect.setAttribute("height", 20);
+
+   this.text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+
+   this.text.setAttribute("fill", Color.BLACK);
+   this.text.setAttribute("fontSize", 15);
+   this.text.setAttribute("fontFamily", "Arial");
+   this.text.setAttribute("fontWeight", "bold");
+
+   this.text.setAttribute("x", 5);
+   this.text.setAttribute("y", 15);
+
+   this.group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+   this.group.appendChild(this.rect);
+   this.group.appendChild(this.text);
+
+   this.x = Tile.SIZE;
+   this.y = Tile.SIZE;
+
+   //this.group.setAttribute("transform", "translate(20, 20)");
+   this.group.setAttribute("transform", ['translate(', this.x, ', ', this.y, ')'].join(''));
+}
+
+function Tile_getPoint() {
+   return this.point;
+}
+
+function Tile_attachTo(svg) {
+   svg.appendChild(this.group);
+}
+
+function Tile_detachFrom(svg) {
+   svg.removeChild(this.group);
+}
+
+function Tile_setXCoordinate(x) {
+   this.x = x;
+   this.group.setAttribute("transform", ["translate(", this.x, ", ", this.y, ")"].join(' '));
+}
+
+function Tile_setYCoordinate(y) {
+   this.y = y;
+   this.group.setAttribute("transform", ["translate(", this.x, ", ", this.y, ")"].join(' '));
+}
+
+Tile.SIZE = 20;
+
+function Tile_onClick(func) {
+   this.group.onclick = function(e) {
+      func.call();
+   }
+}
+
+function Tile_setPoint(point) {
+   this.point = point;
+   this.text.textContent = this.point;
+}
+
+function Tile_getPoint() {
+   return this.point;
+}
+
+function Tile_setColor(color) {
+   this.rect.setAttribute("fill", color);
+}
+
+function Tile_getColor() {
+   return this.rect.getAttribute("fill");
+}
